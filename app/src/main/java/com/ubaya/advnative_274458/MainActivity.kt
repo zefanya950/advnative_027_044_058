15b3efe985f2465fa762ffec2495f747b53aabf8
Jackly Christanto Perdana <62692463+jkl1721@users.noreply.github.com>
2021-08-12 15:06:33 +0700

    Test Push, Added Toast

--- app/src/main/java/com/ubaya/advnative_274458/MainActivity.kt
+++ app/src/main/java/com/ubaya/advnative_274458/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ubaya.advnative_274458
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_toast = findViewById(R.id.btnToast) as Button
+        btn_toast.setOnClickListener {
+            Toast.makeText(this, "Toasted!.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
